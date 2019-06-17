7795c9eae5e60a2dc6b1401db0ed6dd2614c5638
iPulsar <mountaineagleinc@gmail.com>
2019-06-17 10:36:52 +0300

    Initial commit

---
+++ Wave/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Wave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Stop = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(32, 102);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Location = new System.Drawing.Point(32, 190);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(808, 314);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(852, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(133, 102);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Visualisation display:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 608);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Wave/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Un4seen.Bass;
+
+namespace Wave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int n;
+        bool f;
+        string FileNAme = "";
+        int chan = 0;//номер потока
+        Single[] fft = null;//массив данных спектра
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = Bass.BASS_GetDevice();//получаем устройство по умолчанию
+            if (Bass.BASS_Init(n, 44100, 0, IntPtr.Zero) == false) //попытка инициализации
+            {
+                MessageBox.Show("BASS_Init failed");
+                return;
+            }
+            if (FileNAme != "") // Если подобрали свой файл
+            {
+                chan = Bass.BASS_StreamCreateFile(@FileNAme,
+                    0, 0, BASSFlag.BASS_SAMPLE_FLOAT | BASSFlag.BASS_SAMPLE_LOOP);//создаем поток в режиме FLOAT
+            }
+            else // Иначе откроем файл для визуализации по умолчанию.
+            {
+                chan = Bass.BASS_StreamCreateFile(@"C:\MP3\file.mp3",
+                     0, 0, BASSFlag.BASS_SAMPLE_FLOAT | BASSFlag.BASS_SAMPLE_LOOP);//создаем поток в режиме FLOAT
+            }
+    
+            if (chan == 0)
+            {
+                MessageBox.Show("BASS_StreamCreateFile failed");
+                return;
+            }
+
+            f = Bass.BASS_ChannelPlay(chan, false);//воспроизводим поток
+            if (f == false)
+            {
+                MessageBox.Show("BASS_ChannelPlay failed");
+                return;
+            }
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (fft == null) return;
+
+            PointF p1, p2;
+            float max_y = 0;//максимальное значение амплитуды
+            float min_y = Single.MaxValue;//минимальное значение амплитуды
+            float max_x = fft.Length - 1;//максимальная частота, для которой амплитуда ненулевая
+
+            int i = 0;
+            max_x = 0;
+            foreach (float f in fft)//находим максимальные и минимальные значения
+            {
+                if (f > max_y) max_y = f;
+                if (f < min_y) min_y = f;
+
+                if (f > 0.001f) max_x = (float)i;
+                i++;
+            }
+
+            /*меняем направление оси Y, чтобы она смотрела вверх*/
+            e.Graphics.ScaleTransform(1.0f, -1.0f);
+            e.Graphics.TranslateTransform(0.0f, -1.0f * panel1.Height);
+
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+
+            float y;
+            p1 = new PointF(0.0f, 0.0f);//начальная точка
+
+            for (float x = 1; x <= max_x; x++)
+            {
+                y = fft[(int)x];
+
+                /*вычисляем координату следующей точки по относительной амплитуде*/
+                p2 = new PointF((x / max_x) * panel1.Width, panel1.Height * (y - min_y) / (max_y - min_y));
+
+                path.AddLine(p1, p2);//добавляем линию в график
+                p1 = p2;
+            }
+            e.Graphics.DrawPath(Pens.Black, path);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//запускаем каждые 500 мс
+        {
+            if (chan == 0) return;
+            if (Bass.BASS_ChannelIsActive(chan) != BASSActive.BASS_ACTIVE_PLAYING) return;
+
+            fft = new Single[2048];//выделяем массив для данных            
+            Bass.BASS_ChannelGetData(chan, fft, (int)BASSData.BASS_DATA_FFT4096);//получаем спектр потока
+            fft[0] = 0.0f;//избавляемся от постоянной составляющей            
+
+            panel1.Refresh();//перерисовка графика
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Bass.BASS_ChannelStop(chan); //останавливаем музяку
+            Bass.BASS_Free();// выгружаем устройства потоки итд.
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd1 = new OpenFileDialog();
+            if(fd1.ShowDialog().Equals(DialogResult.OK))
+            {
+                FileNAme = fd1.FileName;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
